ae215b358ddbb4741aed58b952bc8567bc2cf694
Arvind <pingarvindforquestions@gmail.com>
2024-04-19 09:53:35 +0000

    Added: ui folder

---
+++ ui/src/config.ts
@@ -0,0 +1,7 @@
+function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+const BASE_URL = getBaseUrl();
